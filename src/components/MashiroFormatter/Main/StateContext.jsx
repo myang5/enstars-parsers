d937e3f22b8d186b817e5882cb85f4e53f3be875
Midori Yang <midori@joinmonument.com>
2021-10-10 19:35:20 -0400

    fix code for getting initial config

--- src/components/MashiroFormatter/Main/StateContext.jsx
+++ src/components/MashiroFormatter/Main/StateContext.jsx
@@ -1,11 +1,18 @@
 import React, { createContext, useState, useRef, useContext } from 'react';
+import { Formatters } from '@constants';
 
 const StateContext = createContext();
 
 export const useStateContext = () => useContext(StateContext);
 
+const initialConfig = JSON.parse(
+  localStorage.getItem(Formatters.MashiroFormatter),
+);
+
 export const StateProvider = ({ children }) => {
-  const [nav, setNav] = useState(JSON.parse(localStorage.getItem('nav')) || {});
+  const [nav, setNav] = useState(
+    initialConfig?.nav || JSON.parse(localStorage.getItem('nav')) || {},
+  );
 
   // create refs for each CKEditor to pass into EditorContext
   const inputRef = useRef(null);
