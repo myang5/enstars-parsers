8fb1254a8fe6aa53c9451e0d059191430d0ab8a8
Midori Yang <midori@joinmonument.com>
2022-12-19 15:53:31 -0500

    toggle inputs based on main story nav

--- src/components/EngirlsWikiFormatter/NavContent/NavContent.jsx
+++ src/components/EngirlsWikiFormatter/NavContent/NavContent.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import { partition } from 'lodash';
 import { Toggle } from '@shared';
 import { useStateContext } from '../StateContext';
 import { NAV_KEYS } from '../utils';
 import './NavContent.less';
 
-const LabelForNavKey = {
-  [NAV_KEYS.STORY_URL]: 'Story URL',
-  [NAV_KEYS.PREV_URL]: 'Prev URL',
-  [NAV_KEYS.PREV_TEXT]: 'Prev Text',
-  [NAV_KEYS.CURRENT_TEXT]: 'Current Text',
-  [NAV_KEYS.NEXT_URL]: 'Next URL',
-  [NAV_KEYS.NEXT_TEXT]: 'Next Text',
+const PropsForNavKey = {
+  [NAV_KEYS.STORY_URL]: { label: 'Story URL', placeholder: 'Ex. Introduction' },
+  [NAV_KEYS.PREV_URL]: { label: 'Prev URL', placeholder: 'Ex. Chapter 5' },
+  [NAV_KEYS.PREV_TEXT]: {
+    label: 'Prev Text',
+    placeholder: 'Ex. 5 - The Rival Candidate - The Living God',
+  },
+  [NAV_KEYS.CURRENT_TEXT]: {
+    label: 'Current Text',
+    placeholder: 'Ex. 6 - The Rival Candidate - The Overlord',
+  },
+  [NAV_KEYS.NEXT_URL]: { label: 'Next URL', placeholder: 'Ex. Chapter 7' },
+  [NAV_KEYS.NEXT_TEXT]: {
+    label: 'Next Text',
+    placeholder: 'Ex. 7 - The Rival Candidate - The Fallen Angel',
+  },
   // Specific to main story
-  [NAV_KEYS.PREV_STORY_URL]: 'Prev Story URL',
-  [NAV_KEYS.NEXT_STORY_URL]: 'Next Story URL',
+  [NAV_KEYS.PREV_STORY_URL]: {
+    label: 'Prev Story URL',
+    placeholder: 'Ex. Introduction',
+  },
+  [NAV_KEYS.NEXT_STORY_URL]: {
+    label: 'Next Story URL',
+    placeholder: 'Ex. Cooperation',
+  },
 };
+
+const [MAIN_STORY_NAV_KEYS, REGULAR_NAV_KEYS] = partition(NAV_KEYS, (key) =>
+  [NAV_KEYS.PREV_STORY_URL, NAV_KEYS.NEXT_STORY_URL].includes(key),
+);
 
 export const NavContent = () => {
   const { nav, setNav, isMainStoryNav, setIsMainStoryNav } = useStateContext();
@@ -25,11 +45,13 @@ export const NavContent = () => {
   };
 
   const handleMainStoryNavToggleChange = (e) => {
-    console.log(e.target.checked);
-    setIsMainStoryNav(e.target.checked);
+    setIsMainStoryNav(!!e.target.checked);
   };
 
-  const keys = Object.values(NAV_KEYS);
+  let keys = Object.values(REGULAR_NAV_KEYS);
+  if (isMainStoryNav) {
+    keys = [...keys, ...MAIN_STORY_NAV_KEYS];
+  }
 
   return (
     <>
@@ -66,8 +88,14 @@ const MainStoryNavToggle = ({ checked, onChange }) => {
 const Row = ({ navKey, value, onChange }) => (
   <>
     <label className="row__label" htmlFor={navKey}>
-      {LabelForNavKey[navKey]}
+      {PropsForNavKey[navKey].label}
     </label>
-    <input type="text" id={navKey} value={value} onChange={onChange} />
+    <input
+      type="text"
+      id={navKey}
+      value={value || ''}
+      onChange={onChange}
+      placeholder={PropsForNavKey[navKey].placeholder}
+    />
   </>
 );
